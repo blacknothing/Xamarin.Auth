1c4f28200c3402c8ae2fd5b470af6191d633e601
Kevin West <west.kevin@gmail.com>
2016-06-20 08:56:50 +0100

    UWP fixes

--- source/Xamarin.Auth.UniversalWindowsPlatform/WebAuthenticatorPage.xaml.cs
+++ source/Xamarin.Auth.UniversalWindowsPlatform/WebAuthenticatorPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace Xamarin.Auth
@@ -14,53 +12,30 @@ namespace Xamarin.Auth
 
         public WebAuthenticatorPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             _auth = (WebRedirectAuthenticator)e.Parameter;
 
-            browser.NavigationStarting += Web_NavigationStarting;
-            browser.NavigationCompleted += Web_NavigationCompleted;
+            browser.NavigationStarting += this.WebNavigationStarting;
+            browser.NavigationCompleted += this.WebNavigationCompleted;
 
-            Uri uri = await _auth.GetInitialUrlAsync();
-            this.browser.Source = uri;
+            var uri = await _auth.GetInitialUrlAsync();
+            browser.Source = uri;
 
-            /*
-            string key = NavigationContext.QueryString["key"];
-
-            this.auth = (WebAuthenticator)PhoneApplicationService.Current.State[key];
-            //this.auth.Completed += (sender, args) => NavigationService.GoBack(); // throws on BackButton
-            this.auth.Completed += auth_Completed;
-            this.auth.Error += OnAuthError;
-
-            PhoneApplicationService.Current.State.Remove(key);
-
-            if (this.auth.ClearCookiesBeforeLogin)
-                await this.browser.ClearCookiesAsync();
-
-            Uri uri = await this.auth.GetInitialUrlAsync();
-            this.browser.Source = uri;
-            */
             base.OnNavigatedTo(e);
         }
 
-        private void Web_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        private void WebNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
         {
             _auth.OnPageLoaded(args.Uri);
         }
 
-        private void Web_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        private void WebNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
         {
             _auth.OnPageLoading(args.Uri);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.GoBack();
-
-            return;
         }
     }
 }
